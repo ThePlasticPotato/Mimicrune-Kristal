349169d56ffd804279d3fc9d241b79f3f9815413
ThePlasticPotato <theplasticpotato@gmail.com>
2026-03-18 20:53:30 -0400

    fix fft crash

--- src/engine/ffthread.lua
+++ src/engine/ffthread.lua
@@ -11,14 +11,16 @@ while true do
     if inChannel:getCount() > 0 then
         -- Get input
         local toFFT = inChannel:pop()
-        -- Perform FFT
-        local res = fft.fft(toFFT, false)
-        -- Process the results to build an intuitive FFT result
-        local fftArray = {}
-        for i = 1, fftSize / 2 do
-            fftArray[i] = postprocess(res[i]:abs() + res[#res-i+1]:abs())
+        if (toFFT) then
+            -- Perform FFT
+            local res = fft.fft(toFFT, false)
+            -- Process the results to build an intuitive FFT result
+            local fftArray = {}
+            for i = 1, fftSize / 2 do
+                fftArray[i] = postprocess(res[i]:abs() + res[#res-i+1]:abs())
+            end
+            -- Send the result to the main thread
+            love.thread.getChannel("fft"):push(fftArray)
         end
-        -- Send the result to the main thread
-        love.thread.getChannel("fft"):push(fftArray)
     end
 end
